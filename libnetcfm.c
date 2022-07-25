e2f7fa37b64352e48cb546fb62037f7c554cded5
Beniamin Sandu <beniaminsandu@gmail.com>
2022-07-26 01:17:54 +0300

    implement a function to get the HW address of an ETH interface
    Signed-off-by: Beniamin Sandu <beniaminsandu@gmail.com>

--- libnetcfm.c
+++ libnetcfm.c
@@ -24,7 +24,50 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+#include <sys/types.h>
+#include <ifaddrs.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <linux/if_packet.h>
+#include <arpa/inet.h>
+
 #include "libnetcfm.h"
+
+int get_eth_mac(char *if_name, uint8_t *mac_addr) {
+    
+    struct ifaddrs *addrs, *ifp;
+    struct sockaddr_ll *sa;
+
+    /* Get a list of network interfaces on the system */
+    if (getifaddrs(&addrs) == -1) {
+        perror("getifaddrs");
+        return EXIT_FAILURE;
+    }
+
+    /* Walk through the list and get the MAC of our interface */
+    ifp = addrs;
+
+    while (ifp != NULL) {
+        if (strcmp(if_name, ifp->ifa_name) == 0) {
+
+            /* We found our interface, copy the HW address */
+            if (ifp->ifa_addr->sa_family == AF_PACKET) {
+                sa = (struct sockaddr_ll *)ifp->ifa_addr;
+                for (int i = 0; i < 8; i++)
+                    mac_addr[i] = sa->sll_addr[i];
+            }
+
+            freeifaddrs(addrs);
+            return EXIT_SUCCESS;
+        }
+        ifp = ifp->ifa_next;
+    }
+
+    /* The provided interface is not present on the machine */
+    freeifaddrs(addrs);
+    return EXIT_FAILURE;
+}
 
 /* Return library version */
 const char *netcfm_lib_version(void) {
